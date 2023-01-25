00af05f0d4485d83c2457f3a0b14d4f58e5cc32a
Caleb <87406555+caleblopez96@users.noreply.github.com>
2023-01-25 11:27:26 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
+// Selectors
 const ball = document.querySelector('.ball');
 const body = document.querySelector('body');
 const widget = document.querySelector('.widget');
 const header = document.querySelectorAll('.headerLink');
 const card = document.querySelectorAll('.card');
 
-
+// invoking night mode when ball is clicked
 ball.addEventListener('click', ()=> {
     nightMode()
 });
 
+// styles for night mode
 const nightMode = () => {
     ball.classList.toggle('nightMode')
     widget.style.backgroundColor = '#fafafa'
@@ -19,18 +21,21 @@ const nightMode = () => {
   /* add white icons as the source for footer links when night mode. */
 };
 
+// function that pushes dayMode styles
 const dayMode = () => {
     ball.classList.toggle('ball')
     body.style.backgroundColor = '#fafafa'
     widget.style.backgroundColor = '#fafafa'
 };
 
+// function that alters header links for night mode.
 const headerNightColors = () => {
     header.forEach(element => {
         element.style.color = '#fafafa'
     })
 };
 
+// function that alters card colors for night mode
 const cardNightColors = () => {
     card.forEach(element => {
         element.style.color = '#252525'
@@ -38,6 +43,7 @@ const cardNightColors = () => {
     })
 };
 
+// function that alters footer colors for night mode
 const footerLinkColors = () => {
     footerLinks.foreach(element => {
         element.style.backgroundColor = '#fafafa'
